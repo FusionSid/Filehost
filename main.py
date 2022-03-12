f0e58c0e34f088afd95e5558397f629d4bccf9f8
Siddhesh Zantye <28945611+FusionSid@users.noreply.github.com>
2022-03-12 07:32:32 +0000

    simple stats

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
+from utils import get_db
+from os import path
+
 limiter = Limiter(key_func=get_remote_address)
 
 # Description for api docs
@@ -42,6 +45,14 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 @app.get("/")
 async def home():
     return RedirectResponse("/docs")
+
+@app.get("/stats")
+async def stats():
+  db_size = path.getsize("utils/database/images.db")
+  return {
+    "images_uploaded" : len((await get_db())),
+    "db" : f"{round((db_size / 1000000), 2)}mb"
+  }
 
 app.include_router(image)
 app.include_router(upload)
